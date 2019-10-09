762708e7a206e4e40d4d43a26cd019a96fed743d
Johannes Wagner <jwagner@audeering.com>
2019-10-09 08:28:00 +0000

    Add __version__ attribute

--- auglib/__init__.py
+++ auglib/__init__.py
@@ -1,3 +1,14 @@
 from .buffer import *
 from .noise import *
 from .tone import *
+
+__version__ = 'unknown'
+
+# Dynamically get the version of the installed module
+try:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution(__name__).version
+except Exception:
+    pkg_resources = None
+finally:
+    del pkg_resources
