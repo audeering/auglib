a90af7d8e55bd56b4e5b195f519b55da4804f71f
Andreas Triantafyllopoulos <atriant@audeering.com>
2019-12-04 09:51:53 +0000

    added test for apply on database

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import os
 import audata
 import shutil
 import audata.testing
@@ -41,4 +42,41 @@ def test_audiomodifier_apply_on_index(duration):
             row['file']) + duration
 
     shutil.rmtree('audio')
+    shutil.rmtree('augmented')
+
+
+@pytest.mark.parametrize('duration', [(5)])
+def test_audiomodifier_apply_on_database(duration):
+    db = audata.testing.create_db(minimal=True)
+    db.schemes['anger'] = audata.Scheme('int', minimum=1, maximum=5)
+    audata.testing.add_table(
+        db,
+        table_id='anger',
+        table_type=audata.define.TableType.FILEWISE,
+        scheme_ids='anger'
+    )
+    audata.testing.create_audio_files(db, root='db')
+    db.save('db')
+
+    transform = AppendValue(duration)
+    t = AudioModifier(transform)
+    t.apply_on_database('db', 'augmented')
+
+    db = audata.Database.load('db')
+    db.map_files(lambda x: os.path.abspath(os.path.join('db', x)))
+    augmented = audata.Database.load('augmented')
+    augmented.map_files(os.path.abspath)
+
+    df = db['anger'].df
+    augmented_df = augmented['anger'].df
+
+    assert len(df) == len(augmented_df)
+
+    for index, row in augmented_df.reset_index().iterrows():
+        original_duration = af.duration(
+            df.index.get_level_values('file')[index])
+        augmented_duration = af.duration(row['file'])
+        assert augmented_duration == original_duration + duration
+
+    shutil.rmtree('db')
     shutil.rmtree('augmented')
