8b725bc51353d89eff6b1db45c246d3737a9c665
Hagen Wierstorf <hwierstorf@audeering.com>
2024-01-29 13:56:36 +0100

    Add comment to code

--- auglib/core/transform.py
+++ auglib/core/transform.py
@@ -3261,10 +3261,14 @@ class PinkNoise(Base):
         def psd(f):
             return 1 / np.where(f == 0, float("inf"), np.sqrt(f))
 
-        white_noise = np.fft.rfft(np.random.randn(samples + 1))
+        # Add extra sample
+        # to ensure correct length for odd samples
+        length = samples + 1
+
+        white_noise = np.fft.rfft(np.random.randn(length))
 
         # Normalized pink noise shape
-        pink_shape = psd(np.fft.rfftfreq(samples + 1))
+        pink_shape = psd(np.fft.rfftfreq(length))
         pink_shape = pink_shape / np.sqrt(np.mean(pink_shape**2))
 
         white_noise_shaped = white_noise * pink_shape
